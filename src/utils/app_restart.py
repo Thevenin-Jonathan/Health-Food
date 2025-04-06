472c45fd52eb70f6f8f6e51aeb946fb675cc03b8
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-04-06 23:08:38 +0200

    Ajout d'une fonction pour redémarrer l'application en fonction de l'environnement d'exécution

---
+++ src/utils/app_restart.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import subprocess
+
+
+def restart_application():
+    """Redémarre l'application"""
+    try:
+        # Si nous sommes dans un environnement PyInstaller
+        if getattr(sys, "frozen", False):
+            # Redémarrer le fichier exécutable
+            os.execv(sys.executable, [sys.executable] + sys.argv)
+        else:
+            # En mode développement, utiliser subprocess
+            args = [sys.executable] + sys.argv
+            subprocess.Popen(args)
+            # Quitter l'application actuelle
+            sys.exit()
+    except Exception as e:
+        print(f"Erreur lors du redémarrage de l'application: {e}")
+        sys.exit(1)
